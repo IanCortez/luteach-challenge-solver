c55aa49ee8a416467a1cecd1047f443ea7b120e5
IanCortez <iancortezg@hotmail.com>
2022-10-20 00:58:15 -0500

    first commit

---
+++ backend/backend/bookings/api/serializers.py
@@ -0,0 +1,24 @@
+from time import time
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from django.utils import timezone
+from ..models import Booking
+
+
+
+class BookingSerializer(ModelSerializer):
+
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+    def validate(self, data):
+        reserved_start_datetime = timezone.datetime.date(data['date'])
+        current_registerd_datetime = timezone.datetime.now().date()
+        if reserved_start_datetime < current_registerd_datetime:
+            raise serializers.ValidationError({"date": "You can't book on a past date"})
+            
+        if data['buyer'] == data['provider']:
+            raise serializers.ValidationError("You can't book yourself")
+
+        return data
---
+++ backend/notes.md
@@ -0,0 +1,13 @@
+# Notas para solucion
+
+## Docker
+
+* `challenge_django` contiene todos los componentes de la carpeta backend
+* `luteach_challenge_app_1` contiene todos los componentes de la carpeta frontend/src
+* `luteach_challenge_web_1` contiene los componentes necesarios para iniciar la aplicacion web usando el container anterior
+* `luteach_challenge_postgres_1` contiene la base de datos y sus herramientas
+
+
+## Backend
+
+* 
