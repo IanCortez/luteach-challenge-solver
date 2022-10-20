b5a30f0728e1b73f4574a43250985b867a3f7c79
IanCortez <iancortezg@hotmail.com>
2022-10-20 01:01:59 -0500

    update: variable names and removed unused imports

--- backend/backend/bookings/api/serializers.py
+++ backend/backend/bookings/api/serializers.py
@@ -14,10 +14,10 @@ class BookingSerializer(ModelSerializer):
 
     def validate(self, data):
         reserved_start_datetime = timezone.datetime.date(data['date'])
-        current_registerd_datetime = timezone.datetime.now().date()
-        if reserved_start_datetime < current_registerd_datetime:
+        current_datetime = timezone.datetime.now().date()
+        if reserved_start_datetime < current_datetime:
             raise serializers.ValidationError({"date": "You can't book on a past date"})
-            
+
         if data['buyer'] == data['provider']:
             raise serializers.ValidationError("You can't book yourself")
 
